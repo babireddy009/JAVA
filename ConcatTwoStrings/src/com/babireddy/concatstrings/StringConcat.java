bfadef41ab9cb574ad7b92222185590425796b8c
sprcses <babireddy009@mail.com>
2020-03-02 17:06:39 +0530

    commit

---
+++ ConcatTwoStrings/src/com/babireddy/concatstrings/StringConcat.java
@@ -0,0 +1,18 @@
+package com.babireddy.concatstrings;
+
+import java.util.Scanner;
+
+public class StringConcat {
+
+	 public static void main(String args[])
+	 {
+	 String str1,str2;
+	 Scanner sc = new Scanner(System.in);
+	 System.out.println("Enter the 1st string");
+	 str1=sc.nextLine();
+	 System.out.println("Enter the 2nd string");
+	 str2=sc.nextLine();
+	   System.out.println("Concatenated String is ");
+	   System.out.println(str1.concat(str2));
+	 }
+	}
